a0c9f1380401b76938cd7de0abdaded6c06593e3
Fa Diaz <diazgonzalez.perla@gmail.com>
2017-06-20 12:54:31 -0400

    autocompletar

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,8 +20,8 @@ function initMap(){
 		var miUbicacion = new google.maps.Marker({
 			position: {lat:latitud, lng:longitud},
 			animation: google.maps.Animation.DROP,
-			map: map
-			icon: 'http://www.gps-routes.co.uk/routes/home.nsf/cycleicon.png'
+			map: map, 
+			icon:'http://www.gps-routes.co.uk/routes/home.nsf/cycleicon.png'
 		});
 		map.setZoom(17);
 		map.setCenter({lat:latitud, lng:longitud});
@@ -30,3 +30,37 @@ function initMap(){
 		alert("Tenemos un problema para encontrar tu ubicación");
 	}
 }
+//autocompletar direcciones
+   	var inputOrigen = (document.getElementById("input-origen"));
+    var autocomplete = new google.maps.places.Autocomplete(inputOrigen);
+        autocomplete.bindTo("bounds", map);
+    var inputDestino = (document.getElementById("input-destino"));
+    var autocomplete = new google.maps.places.Autocomplete(inputDestino);
+        autocomplete.bindTo("bounds", map);
+
+        var directionsDisplay = new google.maps.DirectionsRenderer();
+		var directionsService = new google.maps.DirectionsService();
+		document.getElementById("desde").addEventListener("change", onChangeHandler);
+    	document.getElementById("hasta").addEventListener("change", onChangeHandler);
+    function lineaRuta(directionsService, directionsDisplay) {
+        directionsService.route({
+            origin: document.getElementById("desde").value,
+            destination: document.getElementById('hasta').value,
+            travelMode: "DRIVING"
+            },
+        function(response, status) {
+            if (status === "OK") {
+                directionsDisplay.setDirections(response);
+            } else {
+                window.alert("Ruta no disponible"+ status);
+            }
+        });
+    }
+    	directionsDisplay.setMap(map);
+        //onChangeHandler = Agregar una propiedad de seguimiento a una definición de lenguaje específico de dominio
+        var onChangeHandler = function(){
+            //Servicio de indicaciones
+        lineaRuta(directionsService, directionsDisplay);
+    }; 
+    
+    document.getElementById("ruta").addEventListener("click",onChangeHandler); 
