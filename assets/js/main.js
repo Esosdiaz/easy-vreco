f189c9bcff52e07ce44c857c450239306867518c
Fa Diaz <diazgonzalez.perla@gmail.com>
2017-06-20 13:16:38 -0400

    autocompletado funcionando

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,38 +29,12 @@ function initMap(){
 	var funcionError = function(error){
 		alert("Tenemos un problema para encontrar tu ubicación");
 	}
+	function initialize() {//autocompletar direcciones
+		var inputOrigen = document.getElementById('origen');
+		var autocomplete = new google.maps.places.Autocomplete(inputOrigen);
+		var inputDestino = document.getElementById('destino');
+		var autocompletados = new google.maps.places.Autocomplete(inputDestino);
 }
-//autocompletar direcciones
-   	var inputOrigen = (document.getElementById("input-origen"));
-    var autocomplete = new google.maps.places.Autocomplete(inputOrigen);
-        autocomplete.bindTo("bounds", map);
-    var inputDestino = (document.getElementById("input-destino"));
-    var autocomplete = new google.maps.places.Autocomplete(inputDestino);
-        autocomplete.bindTo("bounds", map);
 
-        var directionsDisplay = new google.maps.DirectionsRenderer();
-		var directionsService = new google.maps.DirectionsService();
-		document.getElementById("desde").addEventListener("change", onChangeHandler);
-    	document.getElementById("hasta").addEventListener("change", onChangeHandler);
-    function lineaRuta(directionsService, directionsDisplay) {
-        directionsService.route({
-            origin: document.getElementById("desde").value,
-            destination: document.getElementById('hasta').value,
-            travelMode: "DRIVING"
-            },
-        function(response, status) {
-            if (status === "OK") {
-                directionsDisplay.setDirections(response);
-            } else {
-                window.alert("Ruta no disponible"+ status);
-            }
-        });
-    }
-    	directionsDisplay.setMap(map);
-        //onChangeHandler = Agregar una propiedad de seguimiento a una definición de lenguaje específico de dominio
-        var onChangeHandler = function(){
-            //Servicio de indicaciones
-        lineaRuta(directionsService, directionsDisplay);
-    }; 
-    
-    document.getElementById("ruta").addEventListener("click",onChangeHandler); 
+google.maps.event.addDomListener(window, 'load', initialize);
+}
